bfde5c5ab6841eff2b4d982982f875df23a78a4b
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-02-06 13:16:22 +0100

    Update sketch.js

--- cv_people/demos/d1/sketch.js
+++ cv_people/demos/d1/sketch.js
@@ -1,5 +1,11 @@
 const QUALITY = "low"
 const CV_PATH = "../../"
+const NUM_H = 16
+const NUM_V = 16
+const SPACING = 120
+const MODEL_SCALE = 350
+const DEF_ROTY_ANGLE = -HALF_PI
+const DEF_OFFS_Y = -0.25 // Unità mesh!
 
 const cv = [
 	"cv1/biazquez_alessandro",
@@ -36,94 +42,73 @@ let drawBouningBox = false
 
 function preload() {
 	font = loadFont("../Inter-Regular.otf")
-
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL)
 	pixelDensity(displayDensity())
 
-
-	for (const head of cv) {
-		console.log ("Loading head: ", head)
-		const headModel = loadModel(`${CV_PATH}/${head}/${QUALITY}/head.obj`, {
-			normalize : false,
-			successCallback: (model) => {
-				headData.aabb = calcBouningBox(model)
-				headData.loaded = true
-			},
-			errorCallback: (error) => {
-				console.error("Error loading model: ", error)
-			}
-		})
-		const headTexture = loadImage(`${CV_PATH}/${head}/${QUALITY}/head.jpg`)
-
-		const headData = {
-			model: headModel,
-			texture: headTexture,
-			aabb: calcBouningBox(headModel),
-			loaded: false,
-			name: head,
-		}
-
-		heads.push(headData)
-	}
-
 	textFont(font)
 	textSize(16)
+
+	loadHeads(heads, cv)
 }
 
 function draw() {
 	background(0)
-	textFont(font)
 
 	push()
 
-	rotateX(map(mouseY, 0, height, -PI/2, PI/2))
-	rotateY(map(mouseX, 0, width,  PI/2, -PI/2))
+	rotateX(map(mouseY, 0, height, -PI*0.3, PI*0.3))
+	rotateY(map(mouseX, 0, width,  PI, -PI))
 
 	if (drawAxes) {
 		const axisLength = min(height, width)
 		drawAxis(axisLength, axisLength, axisLength)
 	}
 
-	const numH = 36
-	const numV = 34
-	const spacing = 140
-	const offsX = -(numH-1) * spacing / 2
-	const offsZ = -(numV-1) * spacing / 2
+	const offsX = -(NUM_H-1) * SPACING / 2
+	const offsZ = -(NUM_V-1) * SPACING / 2
 
-	for (let i = 0; i < numH; i++) {
-		for (let j = 0; j < numV; j++) {
-			index = (i * numV + j) % heads.length
+	for (let i = 0; i < NUM_H; i++) {
+		for (let j = 0; j < NUM_V; j++) {
+			index = (i * NUM_V + j) % heads.length
 			const head = heads[index]
-			const hx = offsX + i * spacing
-			const hz = offsZ + j * spacing
+			const hx = offsX + i * SPACING
+			const hz = offsZ + j * SPACING
+
+			const oy = sin(frameCount * 0.05 + i * 0.4 + j * 0.4) * 50
+			const ry = sin(frameCount * 0.02 + i * 0.1 + j * 0.1) * HALF_PI
 
 			push()
 
-			const oy = sin(frameCount * 0.05 + i * 0.4 + j * 0.4) * 50
+			// Posizionmaneto nello spazio
 			translate(hx, oy, hz)
-			scale(340)
 
-			rotateY(sin(frameCount * 0.02 + i * 0.1 + j * 0.1) * PI)
+			scale(MODEL_SCALE)
+
+			// Tutti i modelli hanno un'altezza diversa,
+			// lo spostamento verticale deve essere calcolato in base all'altezza della bounding box - offset
+			translate(0, head.aabb.height/2 + DEF_OFFS_Y, 0)
+			rotateY(ry + DEF_ROTY_ANGLE)
+
+			push()
+			translate(-head.aabb.x, -head.aabb.y, -head.aabb.z)
+			noStroke()
+			texture(head.texture)
+			textureMode(NORMAL)
+			model(head.model)
+			pop()
+
 
 			if (drawBouningBox){
 				noFill()
 				stroke(255)
-				box(head.aabb.width, head.aabb.height, head.aabb.depth)
+				box(head.aabb.width, head.aabb.height , head.aabb.depth)
 			}
-
 			if (drawAxes) {
-				drawAxis(head.aabb.width, head.aabb.height, head.aabb.depth)
+				drawAxis(head.aabb.width * 1.5, head.aabb.height * 1.5, head.aabb.depth * 1.5)
 			}
-
-
-			translate(-head.aabb.x, -head.aabb.y, -head.aabb.z)
-			texture(head.texture)
-			noStroke()
-			textureMode(NORMAL)
-			model(head.model)
 
 			pop()
 		}
@@ -134,16 +119,14 @@ function draw() {
 	let str = ""
 	for (const head of heads) {
 		if (!head.loaded) {
-			str += "loading: " + head.name + "\n"
+			str += "Loading: " + head.name + "\n"
 		}
 	}
 
 	if (str.length > 0) {
-		camera()
 		translate(-width/2, -height/2)
 		text(str, 10, 30)
 	}
-
 }
 
 function keyPressed() {
@@ -178,6 +161,34 @@ function flipY(model) {
 		vertex.y = -vertex.y
 	}
 	return model
+}
+
+function loadHeads(target, source) {
+	for (const head of source) {
+		console.log ("Loading head: ", head)
+		const headModel = loadModel(`${CV_PATH}/${head}/${QUALITY}/head.obj`, {
+			normalize : false,
+			successCallback: (model) => {
+				headData.model = flipY(model)
+				headData.aabb = calcBouningBox(model)
+				headData.loaded = true
+				console.log (headData.aabb)
+			},
+			errorCallback: (error) => {
+				console.error("Error loading model: ", error)
+			}
+		})
+		const headTexture = loadImage(`${CV_PATH}/${head}/${QUALITY}/head.jpg`)
+
+		const headData = {
+			model: headModel,
+			texture: headTexture,
+			aabb: calcBouningBox(headModel),
+			loaded: false,
+			name: head,
+		}
+		target.push(headData)
+	}
 }
 
 function calcBouningBox(model) {
